692b5270fca3b41443dbfd684955bfe05f3fe017
SubaruHasami <hmsbutterfly16@gmail.com>
2019-01-16 11:34:38 +0630

    make better

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 	
 		require_once("vendor/autoload.php");
 		use Dev\Main\Main as Main;
-		echo $hub_token = trim($_GET["hub_verify_token"]);
-		exit;
+		$hub_token = trim($_GET["hub_verify_token"]);
 		$access_token = "EAACRsbm34AMBACpqadWZAzQZAK4ZCasCChtH6CP32gvgtjKuVRYorfvFNVR8dQOe7k8cIKV9NilOp1WZAfWL1OtsXKZBH91n1bFCB3x5rEfGqYmHVbCE4G64cZBpde42YZBTmq2ziobwctvebN3ZCJVGXaiOW2oQOuwnZAf3BFFl7fDCs6yCNCMJ3";
 		 $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' . $access_token;
 		 $password = "xenovia";
